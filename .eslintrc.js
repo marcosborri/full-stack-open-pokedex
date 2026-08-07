482bbb355cbb6a49a28c66a4b5b4cb47f88618f1
marcosborri <marcosborri06@gmail.com>
2026-08-07 14:54:57 -0300

    fixed lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   'env': {
     'node': true,
+    'browser': true,
     'es6': true,
     'jest/globals': true,
   },
@@ -23,6 +24,7 @@ module.exports = {
       'version': 'detect'
     }
   },
+
   'rules': {
     'indent': [
       'error',
